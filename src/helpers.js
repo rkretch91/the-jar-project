2bed1a89dd70fbab30f48cecbe9800a41ce46889
Ryan Kretch <rkretch91@gmail.com>
2018-10-18 19:33:35 +0200

    working

--- src/helpers.js
+++ src/helpers.js
@@ -3,11 +3,8 @@ export function rando(arr) {
 }
 
 export function getFunName() {
-  const adjectives = ['adorable', 'beautiful', 'clean', 'drab', 'elegant', 'fancy', 'glamorous', 'handsome', 'long', 'magnificent', 'old-fashioned', 'plain', 'quaint', 'sparkling', 'ugliest', 'unsightly', 'angry', 'bewildered', 'clumsy', 'defeated', 'embarrassed', 'fierce', 'grumpy', 'helpless', 'itchy', 'jealous', 'lazy', 'mysterious', 'nervous', 'obnoxious', 'panicky', 'repulsive', 'scary', 'thoughtless', 'uptight', 'worried'];
+  const fears = ['Wear the strangest outfit from your wardrobe in the streets', 'Talk to 5 strangers today', 'Stay up and stare at the stars for 30 mins', 'Purchase tickets (airplane, bus, train) to somewhere you have never gone before', 'Donate $10 to a cause of your desire', 'Smile at 5 people'];
 
-  const nouns = ['women', 'men', 'children', 'teeth', 'feet', 'people', 'leaves', 'mice', 'geese', 'halves', 'knives', 'wives', 'lives', 'elves', 'loaves', 'potatoes', 'tomatoes', 'cacti', 'foci', 'fungi', 'nuclei', 'syllabuses', 'analyses', 'diagnoses', 'oases', 'theses', 'crises', 'phenomena', 'criteria', 'data'];
-
-  const generatedMessage = `${rando(adjectives)}-${rando(adjectives)}-${rando(nouns)}`;
-
+  const generatedMessage = `${rando(fears)}`
   return generatedMessage;
 }
