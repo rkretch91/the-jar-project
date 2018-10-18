28d07d3a8bf91e4ab7d656b1e3d2867f698d9078
Ryan Kretch <rkretch91@gmail.com>
2018-10-19 00:47:12 +0200

    jar project done for now

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,8 @@ export function rando(arr) {
 }
 
 export function getFunName() {
-  const fears = ['Wear the strangest outfit from your wardrobe in the streets', 'Talk to 5 strangers today', 'Stay up and stare at the stars for 30 mins', 'Purchase tickets (airplane, bus, train) to somewhere you have never gone before', 'Donate $10 to a cause of your desire', 'Smile at 5 people'];
+  const fears = ['Wear the strangest outfit from your wardrobe in the streets', 'Talk to 5 strangers today', 'Stay up and stare at the stars for 30 mins', 'Purchase tickets (airplane, bus, train) to somewhere you have never gone before', 'Donate $10 to a cause of your desire', 'Smile at 5 people', 'Hug 5 random people', 'Call your father', 'Call one of your siblings', 'Dance to your favorite song and scream the lyrics outloud', 'Smile and look at 5 people in the eyes', 'Buy a coffee for the person behind you in line', 'Sign up to volunteer at an event...today', 'Give someone you barely know a hug', 'Take your camera out and photograph the world for 1 hour', 'Sign up to volunteer somewhere for 1 hour (if not today, this week)', 'Make cookies for a neighbor', 'Sign up for an event/go to a networking event', 'Send in an appication for something (a job, university, other program)', 'Talk to a stranger for 10 minutes', 'Surprise someone with a special treat',
+  'Spend 30 minutes learning a new language', 'Draw or paint for 1 hour', 'Begin learning a new skill', 'Call someone you are nervous to make a call to', 'Go to a networking event', 'Flirt with someone', 'Talk to 5 random strangers', 'Ask the nearest person to you at this moment for something', 'Tell someone they are pretty/handsome', 'Find one inspiring person and call or write them', 'Turn off your phone for the whole day (or if impossible, then once this week', 'Call a childhood friend'];
 
   const generatedMessage = `${rando(fears)}`
   return generatedMessage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import jar from '../images/jar-image.jpg'
 
 import Layout from '../components/layout'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div class="about">
+      <h1>About</h1>
+      <p>I turned 27 this year, and since my mom and sister were in Berlin for my birthday, I told them to help me with something important: fill a jar with post-it notes of daily tasks that will help me conquer my fears, do things that are out of the ordinary, and make me be nicer to people.</p>
+      <img src={jar} alt="jar-project"/>
+      <p>Things haven’t been easy leading up to that day. I’ve never felt more afraid of rejection, I’ve never felt more disconnected from real people because of technology, and I’ve never felt so detached from myself. My anxiety has been so bad most mornings, that I’ve often contemplated ways of forcing food poisoning or other freak illnesses upon myself to avoid walking out into the world. I have it so freaking good, yet I find myself paralyzed by these demons so much of the time.</p>
+      <p>That got me thinking, inactivity is just going to exacerbate these feelings, but a little jar with daily tasks to spice my days up a bit can only get me closer to my true purpose on this bitch of a planet. I am convinced that the path to happiness and purpose is living a daily existence and balance of KINDNESS, SPONTANEITY, and CONQUERING FEARS.</p>
+      <p>The Jar Project is a daily reminder to open up your world to these three aspects, and do something atypical on the daily; for it is these uncomfortable moments and periods of reflection that will help us reconnect and grow as a human race.</p>
+      <p>Go ahead and pick something out of the jar. Record it, document it, and tomorrow, pick something new. Just think of what a year or lifetime of kindness, spontaneity, and conquering fears can do to you. </p>
+      <Link to="/">Go back to The Jar</Link>
+    </div>
   </Layout>
 )
 
