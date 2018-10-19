4fc029fb1f0e7b3a16571d99447d31c7f7cd9b76
Ryan Kretch <rkretch91@gmail.com>
2018-10-19 14:21:09 +0200

    form configured with netlify

--- src/components/form.js
+++ src/components/form.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Form = () => (
-    <div className="form" data-netlify="true">
-      <form>
+    <div className="form">
+      <form data-netlify="true">
         <input type="text"/>
         <button type="submit" className="btn-form" defaultValue="Submit your own">Add To Jar</button>
       </form>
