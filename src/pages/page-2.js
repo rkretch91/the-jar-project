25d071f43d392fe311c850fe6140b1608eba1b0c
Ryan Kretch <rkretch91@gmail.com>
2018-10-19 14:03:31 +0200

    diagram added

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import jar from '../images/jar-image.jpg'
+import diagram from '../images/diagram.png'
+
 
 import Layout from '../components/layout'
 
@@ -12,6 +14,7 @@ const SecondPage = () => (
       <img src={jar} alt="jar-project"/>
       <p>Things haven’t been easy leading up to that day. I’ve never felt more afraid of rejection, I’ve never felt more disconnected from real people because of technology, and I’ve never felt so detached from myself. My anxiety has been so bad most mornings, that I’ve often contemplated ways of forcing food poisoning or other freak illnesses upon myself to avoid walking out into the world. I have it so freaking good, yet I find myself paralyzed by these demons so much of the time.</p>
       <p>That got me thinking, inactivity is just going to exacerbate these feelings, but a little jar with daily tasks to spice my days up a bit can only get me closer to my true purpose on this bitch of a planet. I am convinced that the path to happiness and purpose is living a daily existence and balance of KINDNESS, SPONTANEITY, and CONQUERING FEARS.</p>
+      <img src={diagram} alt="happiness-diagram"/>
       <p>The Jar Project is a daily reminder to open up your world to these three aspects, and do something atypical on the daily; for it is these uncomfortable moments and periods of reflection that will help us reconnect and grow as a human race.</p>
       <p>Go ahead and pick something out of the jar. Record it, document it, and tomorrow, pick something new. Just think of what a year or lifetime of kindness, spontaneity, and conquering fears can do to you. </p>
       <Link to="/">Go back to The Jar</Link>
