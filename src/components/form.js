8ebb85f4dd1ae7cf1b9622c7084c2712cf7e3909
Ryan Kretch <rkretch91@gmail.com>
2018-10-19 14:18:15 +0200

    added form

---
+++ src/components/form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => (
+    <div className="form" netlify>
+      <form>
+        <input type="text"/>
+        <button type="submit" className="btn-form" defaultValue="Submit your own">Add To Jar</button>
+      </form>
+    </div>
+)
+
+export default Form
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import Form from '../components/form'
 import jar from '../images/the-jar-project-big.png'
 import { getFunName } from '../helpers'
 
@@ -26,6 +27,7 @@ class IndexPage extends React.Component {
         <div className="main-jar" >
           <img src={jar} alt="jar-project-logo" onClick={this.grabNameValue}/>
           <p>Instructions: Click the jar once a day for your daily task. Each task will encourage you to conquer fears, do something nice for others, or encourage spontaneity. Do it, record it, repeat (unless it isn't relevant of course!)</p>
+          <Form/>
           <Link to="/page-2">About The Jar Project</Link>
         </div>
 
