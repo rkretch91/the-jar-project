acd5fb8bbc357edec5cb645b77bf7df671d89214
Ryan Kretch <rkretch91@gmail.com>
2018-10-19 14:37:32 +0200

    form fixed

--- src/components/form.js
+++ src/components/form.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Form = () => (
     <div className="form">
-      <form netlify>
+      <form name="add-to-jar-form" method="post" data-netlify="true">
+        <input type="hidden" name="form-name" value="add-to-jar-form" />
         <input type="text"/>
         <button type="submit" className="btn-form" defaultValue="Submit your own">Add To Jar</button>
       </form>
