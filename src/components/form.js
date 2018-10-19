dbfa49f87608a86250f2ae5aee097156861fb564
Ryan Kretch <rkretch91@gmail.com>
2018-10-19 15:00:44 +0200

    form

--- src/components/form.js
+++ src/components/form.js
@@ -4,9 +4,9 @@ class Form extends React.Component{
   render() {
     return(
       <form name="add" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="bot-field"/>
         <input type="text" name="new" />
-        <button type="submit" className="btn-form" defaultValue="Submit your own">Add To Jar</button>
+        <button type="submit" className="btn-form">Add To Jar</button>
+        <input type="hidden" name="form-name" value="add" />
       </form>
       )
     }
