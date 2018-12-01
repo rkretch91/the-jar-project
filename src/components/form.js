f1269e3f30726adb3798132154e961b71747ec14
Ryan Kretch <rkretch91@gmail.com>
2018-12-01 14:30:51 +0100

    cool

--- src/components/form.js
+++ src/components/form.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-class Form extends React.Component{
-  render() {
-    return(
+const Form = () => (
       <form name="add" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="form-name" value="add" />
+        <input type="hidden" name="bot-field" value="add" />
         <div>
-         <input type="text" name="new" />
+         <input type="text" name="new" id="new" />
         </div>
         <div>
          <input type="submit" className="btn-form" value="[Add To Jar]"/>
         </div>
       </form>
       )
-    }
-}
 
 export default Form
