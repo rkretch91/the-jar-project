8ac246927b5bd46e550406b5a77d91d4e2c64e8b
Ryan Kretch <rkretch91@gmail.com>
2018-12-01 14:38:56 +0100

    dfd

--- src/components/form.js
+++ src/components/form.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Form = () => (
+    <div>
       <form name="add" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="bot-field"/>
         <div>
@@ -10,6 +11,7 @@ const Form = () => (
          <input type="submit" className="btn-form" value="[Add To Jar]"/>
         </div>
       </form>
+    </div>
       )
 
 export default Form
