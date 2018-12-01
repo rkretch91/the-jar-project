b6eba35aacf3c10b81249a41260741340834d96b
Ryan Kretch <rkretch91@gmail.com>
2018-12-01 14:52:04 +0100

    done

--- src/components/form.js
+++ src/components/form.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Form = () => (
     <div>
       <form name="add" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field"/>
         <div>
          <input type="text" name="new" id="new" />
         </div>
         <div>
          <input type="submit" className="btn-form" value="Add To Jar"/>
         </div>
-        <input type="hidden" name="bot-field"/>
       </form>
     </div>
       )
