c839dd244a09cfacf8fb5f48659e34721fa5c919
Ryan Kretch <rkretch91@gmail.com>
2018-12-27 21:53:58 +0100

    done

--- src/components/form.js
+++ src/components/form.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Form = () => (
     <div className="field">
-      <form name="add" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="bot-field"/>
+      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" name="form-name"/>
         <div>
          <input type="text" name="new" id="new" />
         </div>
