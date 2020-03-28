6298093189d58889712a2eb571e8d139b69ca1c5
Ryan Kretch <rkretch91@gmail.com>
2020-03-28 13:21:09 +0100

    changed jar contents

--- src/components/form.js
+++ src/components/form.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
 const Form = () => (
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <input type="hidden" name="bot-field" name="form-name"/>
-        <div>
-         <input type="text" name="new" id="new" />
-        </div>
+      <form name="add-form" action="/" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="bot-field" />
+        <input type="text" name="new" id="new" />
         <input type="submit" className="btn-form button" value="Add To Jar"/>
       </form>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -39,11 +39,11 @@ class IndexPage extends React.Component {
         </div>
         <div className="main-jar">
           <img src={jar} alt="jar-project-logo" onClick={this.grabNameValue}/>
-          <p>Instructions: Click the jar once a day for your daily task. Each task will encourage you to conquer fears, do something nice for others, or encourage spontaneity. Do it, record it, repeat (unless it isn't relevant of course!)</p>
+          <p>Instructions: Click the jar once a day for your daily digital task. Each task will encourage you to do something nice for others during this time when we need it. Do it, record it, repeat.</p>
         </div>
         <div className="main-jar">
           <Form/>
-          <Link to="/page-2">About The Jar Project</Link>
+          <Link to="/about">About The Jar Project</Link>
         </div>
         <Insta/>
       </Layout>
